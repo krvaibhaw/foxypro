f7bcae8d62efbd140a516464665713e2a9a47de9
Vaibhaw <kvaibhaw99@gmail.com>
2022-03-28 20:02:12 +0530

    Added parser for  background process

--- myshell/background.py
+++ myshell/background.py
@@ -1,2 +1,7 @@
 import subprocess
 import shlex
+
+def handle_background(command):
+    parts = shlex.split(command)
+    process = subprocess.Popen(parts)
+    print(f"Started process {process.pid} in background")
