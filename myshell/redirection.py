f90b54b56a7a7b364092379cb7f0d7f2a2436b98
Vaibhaw <kvaibhaw99@gmail.com>
2022-04-21 20:10:45 +0530

    Handling forward/output redirection

--- myshell/redirection.py
+++ myshell/redirection.py
@@ -1,2 +1,11 @@
 import subprocess
 import shlex
+
+ef handle_redirection(command):
+    parts = shlex.split(command)
+    if '>' in parts:
+        index = parts.index('>')
+        filename = parts[index + 1]
+        parts = parts[:index]
+        with open(filename, 'w') as f:
+            subprocess.run(parts, stdout=f)
