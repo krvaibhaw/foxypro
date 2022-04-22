87b4cce217b6862872f55430f39f79e6242441e9
Vaibhaw <kvaibhaw99@gmail.com>
2022-04-22 20:11:35 +0530

    Handling forward/output redirection V2

--- myshell/redirection.py
+++ myshell/redirection.py
@@ -9,3 +9,9 @@ ef handle_redirection(command):
         parts = parts[:index]
         with open(filename, 'w') as f:
             subprocess.run(parts, stdout=f)
+    lif '>>' in parts:
+        index = parts.index('>>')
+        filename = parts[index + 1]
+        parts = parts[:index]
+        with open(filename, 'a') as f:
+            subprocess.run(parts, stdout=f)
