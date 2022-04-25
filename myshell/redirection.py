94155144952aac9a24e9bb8d5a8a0865eaea04aa
Vaibhaw <kvaibhaw99@gmail.com>
2022-04-25 20:13:54 +0530

    Handling backward/input redirection

--- myshell/redirection.py
+++ myshell/redirection.py
@@ -1,7 +1,7 @@
 import subprocess
 import shlex
 
-ef handle_redirection(command):
+def handle_redirection(command):
     parts = shlex.split(command)
     if '>' in parts:
         index = parts.index('>')
@@ -15,3 +15,11 @@ ef handle_redirection(command):
         parts = parts[:index]
         with open(filename, 'a') as f:
             subprocess.run(parts, stdout=f)
+    elif '<' in parts:
+        index = parts.index('<')
+        filename = parts[index + 1]
+        parts = parts[:index]
+        with open(filename, 'r') as f:
+            subprocess.run(parts, stdin=f)
+    else:
+        subprocess.run(parts)
