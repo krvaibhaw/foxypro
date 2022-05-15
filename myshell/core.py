5670e47be1210b07cd3c2cbc0082423772b29f2b
Vaibhaw <kvaibhaw99@gmail.com>
2022-05-15 22:16:00 +0530

    LInking dependency package

--- myshell/core.py
+++ myshell/core.py
@@ -1,14 +1,34 @@
+from myshell.redirection import handle_redirection
+from myshell.background import handle_background
+from myshell.utils import parse_command
+
 def main_loop():
+    print("\n")
+    print(" ********                                                  ");
+    print("/**/////                    **   ** ******                 ");
+    print("/**        ******  **   ** //** ** /**///** ******  ****** ");
+    print("/*******  **////**//** **   //***  /**  /**//**//* **////**");
+    print("/**////  /**   /** //***     /**   /******  /** / /**   /**");
+    print("/**      /**   /**  **/**    **    /**///   /**   /**   /**");
+    print("/**      //******  ** //**  **     /**     /***   //****** ");
+    print("//        //////  //   //  //      //      ///     //////  ");
+    print("\n")
+    print("////////////////////////////////////////////////////////////");
+    print("************************************************************");
+    print("\n")
     print("Welcome to Foxypro Shell :)\n")
-    while True:
+
+    while True:        
         user_input = input("$ ")
         if user_input == "exit":
             break
         process_command(user_input)
-        
+
 def process_command(command):
+    
     background, command = parse_command(command)
     if background:
         handle_background(command)
     else:
         handle_redirection(command)
+        
