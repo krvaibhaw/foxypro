da1bcaf0efbc9bf4f91b23b05f14b8f8a2a02414
Vaibhaw <kvaibhaw99@gmail.com>
2024-04-08 04:45:00 +0530

    feat: add BANNER ASCII art constant

--- myshell/core.py
+++ myshell/core.py
@@ -6,3 +6,14 @@ from myshell.background import handle_background
 from myshell.utils import parse_command
 from myshell.builtins import execute_builtin, is_builtin, add_to_history, load_aliases, save_aliases
 from myshell.validation import validate_command, print_error
+
+BANNER = r"""
+ ********                                                  
+/**/////                    **   ** ******                 
+/**        ******  **   ** //** ** /**///** ******  ****** 
+/*******  **////**//** **   //***  /**  /**//**//* **////**
+/**////  /**   /** //***     /**   /******  /** / /**   /**
+/**      /**   /**  **/**    **    /**///   /**   /**   /**
+/**      //******  ** //**  **     /**     /***   //****** 
+//        //////  //   //  //      //      ///     //////  
+"""
